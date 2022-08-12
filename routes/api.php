b60049a88c647d7c5a3a125fe0bbdff4882d4406
duro06 <andi.meka.025@gmail.com>
2022-08-13 00:18:26 +0700

    nambah JWTMiddleware, ngasih nama route di api.php

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
